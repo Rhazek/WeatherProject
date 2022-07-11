3615f7308877fc5702b23aaa796f88b29f87018f
RHAZEK <matheus_rhazek@hotmail.com>
2022-07-11 18:59:34 -0300

    updating

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ app.get("/", function(req, res){
       const city = weatherData.name;
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
       res.write("<p>The wether is currently " + weatherDescription + "</p>");
       res.write("<h1>The temperature in " + city + " is " + temp + " degrees Celsius.</h1>");
+      res.write("<img src=" + imageURL + ">");
       res.send();
     })
   });
